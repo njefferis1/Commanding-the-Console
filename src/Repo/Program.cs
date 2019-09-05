e3977909611e6f2d068691f190ebb2c1346364f1
njefferis1 <46577146+njefferis1@users.noreply.github.com>
2019-09-05 11:37:28 -0600

    Closes #1

---
+++ src/Repo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http; // for HttpClient
+using static System.Console; // Allows me to directly call static methods on the Console class, wothout the classname qualifier
+
+namespace Repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hey bitches");
+        }
+    }
+}
